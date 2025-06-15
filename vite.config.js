57cef2f2ee48e8fec75ea9717c8791757bc27b81
hamzatariq3 <108178486+hamzatariq3@users.noreply.github.com>
2025-06-16 00:49:20 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <-- This line is critical for Netlify to render your React app properly
+  base: './',
   plugins: [react()],
 })
