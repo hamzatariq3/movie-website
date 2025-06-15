9234cb1f0a462ccfdd99b31212243201e0255e35
hamzatariq3 <108178486+hamzatariq3@users.noreply.github.com>
2025-06-16 00:46:10 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <-- This line is critical for Netlify to render your React app properly
   plugins: [react()],
-  base: './', // ✅ This is the key fix for Netlify blank screen
 })
